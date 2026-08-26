b600c5d12c1b133072a728e2a2bc4abe20d056ec
zl1nq <1246835264@qq.com>
2026-08-26 10:45:49 +0800

    agent方面小调整

--- vibe-music-backend/vibe-music-server/src/main/java/com/vibemusic/controller/agent/AgentController.java
+++ vibe-music-backend/vibe-music-server/src/main/java/com/vibemusic/controller/agent/AgentController.java
@@ -1,10 +1,10 @@
 package com.vibemusic.controller.agent;
 
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
-import com.vibemusic.agent.AgentPrompt;
 import com.vibemusic.agent.MusicAgentTools;
 import com.vibemusic.model.dto.AgentChatDTO;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
@@ -19,7 +19,7 @@ import reactor.core.publisher.Flux;
 
 import static com.vibemusic.agent.AgentPrompt.DEFAULT_PROMPT;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/agent")
 public class AgentController {
@@ -28,9 +28,8 @@ public class AgentController {
     private final ChatClient dashScopeChatClient;
 
     public AgentController(ChatClient.Builder chatClientBuilder,
-                           @Qualifier("dashScopeChatModel") ChatModel chatModel,
                            MusicAgentTools musicAgentTools) {
-
+        log.info("AgentController constructor");
         this.dashScopeChatClient = chatClientBuilder
                 .defaultSystem(DEFAULT_PROMPT)
                 .defaultAdvisors(
@@ -48,11 +47,11 @@ public class AgentController {
     }
 
     /**
-     * VibeAgent 聊天接口（流式，SSE 输出），供前端聊天窗使用
+     * VibeAgent 聊天接口，供前端聊天窗使用
      */
     @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<String>> chat(@RequestBody AgentChatDTO dto, HttpServletResponse response) {
-
+        log.info("chatController chat");
         response.setCharacterEncoding("UTF-8");
 
         Flux<String> content = this.dashScopeChatClient.prompt(dto.getQuery())
